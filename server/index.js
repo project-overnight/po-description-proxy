ed41abd20634248d952a86449e430a1166c93a8e
Jesse Beard <jjbeard@protonmail.com>
2020-05-13 23:50:51 -0700

    TODO: styling

--- server/index.js
+++ server/index.js
@@ -1,29 +1,21 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 9966;
-const BANNER = 'http://localhost:9800/';
-const DESCRIPTION = 'http://localhost:9006/';
-const RESERVATIONS = 'http://localhost:9001/';
-const REVIEWS = 'http://localhost:9999/';
+const HOST = 'localhost'
+const BANNER = `http://${HOST}:9800/`;
+const DESCRIPTION = `http://${HOST}:9006/`;
+const RESERVATIONS = `http://${HOST}:9001/`;
+const REVIEWS = `http://${HOST}:9999/`;
 
 
-app.listen(PORT, () => { console.log(`Listening on port http://localhost:${PORT}/?1`); });
-
+app.use(createProxyMiddleware(`${BANNER}api/photos`, { changeOrigin: true }));
+app.use(createProxyMiddleware(`${DESCRIPTION}api/description`, { changeOrigin: true }));
+app.use(createProxyMiddleware(`${RESERVATIONS}api/room`, { changeOrigin: true }));
+app.use(createProxyMiddleware(`${REVIEWS}api/reviews`, { changeOrigin: true }));
 app.use('/', express.static('public'));
 app.use(express.urlencoded());
 
-
-app.all('/photos/:id', (req, res) => {
-  const response = Response.redirect(`${BANNER}api/photos/${req.params.id}`);
-  console.log('here', res)
-  fetch(response)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    });
-});
+app.listen(PORT, () => { console.log(`Listening on port http://${HOST}:${PORT}/?1`); });
 
